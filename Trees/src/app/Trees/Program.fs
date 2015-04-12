f46717120cf14d42628b781a3d88ee177a048854
bjartwolf <bjartnes@gmail.com>
2015-04-12 21:06:31 +0200

    Added program to trees

--- Trees/src/app/Trees/Program.fs
+++ Trees/src/app/Trees/Program.fs
@@ -1,7 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
-
+open Trees
+open System 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let tree = flip(Split (Bud, LeftFruit(Some Fig, Bud)))
+    printfn "%A" tree 
+    Console.ReadLine() |> ignore
     0 // return an integer exit code
