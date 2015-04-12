45df36fc7b6e0f7b97283ddac369f0c3793c8793
Glorp <jonas.winje@gmail.com>
2015-04-12 20:40:44 +0200

    fix path

--- Trees/build.sh
+++ Trees/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mono .nuget/nuget.exe restore
-mono .nuget/nuget.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
+mono .nuget/NuGet.exe restore
+mono .nuget/NuGet.exe install FAKE -OutputDirectory tools -ExcludeVersion -Prerelease
 mono tools/FAKE/tools/FAKE.exe --fsiargs -d:MONO build.fsx
 
