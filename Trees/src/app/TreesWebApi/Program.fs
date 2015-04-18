fe1cf6ffaf8f3dcd95266ff4759f0fe963cd63fc
Glorp <jonas.winje@gmail.com>
2015-04-18 14:18:23 +0200

    created webapi

---
+++ Trees/src/app/TreesWebApi/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open Microsoft.Owin.Hosting
+
+[<EntryPoint>]
+let main argv = 
+
+    let baseAddress = "http://localhost:8888"
+
+    use application = WebApp.Start<Startup.Startup>(baseAddress)
+
+    Console.WriteLine("Server running on {0}", baseAddress)
+    Console.WriteLine("hit <enter> to stop")
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
---
+++ Trees/src/app/TreesWebApi/Startup.fs
@@ -0,0 +1,33 @@
+﻿module Startup
+
+    open Owin
+    open Microsoft.Owin
+    open System
+    open System.IO
+    open System.Threading.Tasks
+    open Trees
+
+    type public Startup() = 
+        let t1 = LeftFruit (Some Fig, Bud)
+        let handleOwinContext (context:IOwinContext) =
+
+            use writer = new StreamWriter(context.Response.Body)
+
+            match context.Request.Method with
+            | "GET" -> 
+                use reader = new StreamReader(context.Request.Body)
+                let rec printtree tree = match tree with
+                                        | Bud -> "Grein"
+                                        | LeftFruit (f, t) -> "Frukt" +  printtree t
+                                        | RightFruit (t,f) -> "Frukt" +  printtree t
+                                        | Split (t1, t2) ->  printtree t1 + printtree t2
+                writer.Write(printtree t1)
+            | _ ->
+                context.Response.StatusCode <- 400
+                writer.Write("Only GET")
+
+        let owinHandler = fun (context:IOwinContext) (_:Func<Task>) -> 
+            handleOwinContext context; 
+            Task.FromResult(null) :> Task
+
+        member x.Configuration (app:IAppBuilder) = app.Use(owinHandler) |> ignore
