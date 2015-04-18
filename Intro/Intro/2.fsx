adee819154f9a5e584e7b86863f2647f9c0f421b
Glorp <jonas.winje@gmail.com>
2015-04-18 13:43:51 +0200

    renamed a few things

--- Intro/Intro/2.fsx
+++
@@ -1,7 +0,0 @@
-﻿// By separating things with | we can make more things in a type
-
-type seasoning = Salt | Pepper
-let seasonings = [Salt ; Pepper ; Pepper; Salt]
-seasonings
-
-// Try making something with three or four different things
