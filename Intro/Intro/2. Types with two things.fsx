c583ac0dbfd303b042831e298755127530a21feb
Glorp <jonas.winje@gmail.com>
2015-04-18 13:44:01 +0200

    renamed a few things

---
+++ Intro/Intro/2. Types with two things.fsx
@@ -0,0 +1,7 @@
+﻿// By separating things with | we can make more things in a type
+
+type seasoning = Salt | Pepper
+let seasonings = [Salt ; Pepper ; Pepper; Salt]
+seasonings
+
+// Try making something with three or four different things
