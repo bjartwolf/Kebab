09bb11ad31bf4e4236b495fce70a2c7ac166c0c7
Teodor Ande Elstad <teodor@andeelstad.com>
2017-02-27 18:33:30 +0100

    Add Queens and Goldback meep moop map

---
+++ assignments/8. Queens.fsx
@@ -0,0 +1,41 @@
+// alt + enter to send line or selection to fsi/repl
+// Reset fsi by clicking on "Kill Terminal" (the garbage can icon) in the VS Code terminal
+
+
+exception Nope
+let wa _ = raise Nope
+let t = ()
+
+(*
+    Have you ever heard about the eight queens puzzle? (https://en.wikipedia.org/wiki/Eight_queens_puzzle)
+
+    invalidPositions should compare two (x, y)-tuples with positions for two queens,
+    and return true if they are attacking each other
+    
+    validSolution takes a (queen, board)-tuple as arguments
+        the queen is an (x, y)-tuple with the position of a queen
+        the board is a list of (x, y)-tuples for already positioned queens
+    it should return true if it is fine to add the queen to the board
+    (if the new queen is not attacking any of the queens already positioned)
+    
+    newBoards takes a y and a board as arguments
+        the y is a column number, an int
+        the board is a list of (x, y)-tuples positioned queens
+    it should:
+        make new boards by adding queens to different rows in the y column
+            (i.e. by making new (x, y)-tuples for possible positions for the new queen)
+        and return the new boards that do not have any queens attacking each other
+*)
+
+let queens n =
+    let invalidPosition (x1, y1) (x2, y2) = (wa t) // todo
+    let validSolution (queen, board) = not (Seq.exists (invalidPosition queen) board)
+
+    let newBoards y board : (int * int) list seq = (wa t) // todo
+
+    let allNewBoards boards y = Seq.collect (newBoards y) boards
+    Seq.fold allNewBoards (Seq.singleton []) (seq { 1 .. n })
+
+queens 4
+
+Seq.length (queens 8)
---
+++ assignments/9. Goldbach.fsx
@@ -0,0 +1,26 @@
+// alt + enter to send line or selection to fsi/repl
+// Reset fsi by clicking on "Kill Terminal" (the garbage can icon) in the VS Code terminal
+
+
+exception Nope
+let wa _ = raise Nope
+let t = ()
+
+(*
+    Goldbach's conjecture (https://en.wikipedia.org/wiki/Goldbach%27s_conjecture)
+    states that every even integer greater than 2 can be expressed as the sum of two primes.
+
+    The goldback function below takes an integer n and returns a tuple (p1, p2) of two primes where p1 + p2 = n.
+    What should happen if 1, 2 or an odd number is given as argument for the function?
+    
+    isPrime takes a number and determines if it is a prime or not. findPrimeTuple takes a number d,
+    and returns the tuple (d, n-d) if both the numbers are primes, otherwise it calls itself with another d.
+*)
+
+let goldbach n : int * int = 
+    let isPrime n : bool = wa t
+    let rec findPrimeTuple d  : int * int = wa t
+    findPrimeTuple 2
+
+goldbach 28
+goldbach 35
