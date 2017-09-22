3e10b74f21ae06a4c3426031977c2351944067bd
Jonas <jonas.winje@gmail.com>
2017-09-22 15:11:03 +0200

    tabs

--- assignments/8. Queens.fsx
+++ assignments/8. Queens.fsx
@@ -7,24 +7,24 @@ let wa _ = raise Nope
 let t = ()
 
 (*
-    Have you ever heard about the eight queens puzzle? (https://en.wikipedia.org/wiki/Eight_queens_puzzle)
+  Have you ever heard about the eight queens puzzle? (https://en.wikipedia.org/wiki/Eight_queens_puzzle)
 
-    invalidPositions should compare two (x, y)-tuples with positions for two queens,
-    and return true if they are attacking each other
+  invalidPositions should compare two (x, y)-tuples with positions for two queens,
+  and return true if they are attacking each other
     
-    validSolution takes a (queen, board)-tuple as arguments
-        the queen is an (x, y)-tuple with the position of a queen
-        the board is a list of (x, y)-tuples for already positioned queens
-    it should return true if it is fine to add the queen to the board
-    (if the new queen is not attacking any of the queens already positioned)
+  validSolution takes a (queen, board)-tuple as arguments
+    the queen is an (x, y)-tuple with the position of a queen
+    the board is a list of (x, y)-tuples for already positioned queens
+  it should return true if it is fine to add the queen to the board
+  (if the new queen is not attacking any of the queens already positioned)
     
-    newBoards takes a y and a board as arguments
-        the y is a column number, an int
-        the board is a list of (x, y)-tuples positioned queens
-    it should:
-        make new boards by adding queens to different rows in the y column
-            (i.e. by making new (x, y)-tuples for possible positions for the new queen)
-        and return the new boards that do not have any queens attacking each other
+  newBoards takes a y and a board as arguments
+    the y is a column number, an int
+    the board is a list of (x, y)-tuples positioned queens
+  it should:
+    make new boards by adding queens to different rows in the y column
+      (i.e. by making new (x, y)-tuples for possible positions for the new queen)
+      and return the new boards that do not have any queens attacking each other
 *)
 
 let queens n =
